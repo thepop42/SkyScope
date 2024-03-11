9f088d55eb0d3b44e1da8a47cc1b6d4e64041485
Margarita Sanz <37813342+Msanzdelrio@users.noreply.github.com>
2024-03-11 17:30:18 +0000

    Refactor city retrieval in index() function

--- app/views.py
+++ app/views.py
@@ -7,14 +7,15 @@ def index():
     weather = None
 
     if request.method == 'POST':
-        city = request.form.get('city')
+        # Get the city from the form
+        city = request.form['city']
 
         url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid=b5268fcd2ca37ab2198170fac2825453'
 
         response = requests.get(url).json()
 
         weather = {
-            'city': city,
+            'country': response['sys']['country'],
             'temperature': response['main']['temp'],
             'description': response['weather'][0]['description'],
             'icon': response['weather'][0]['icon'],
