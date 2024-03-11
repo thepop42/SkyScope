06183e6a5d28f464cd4b8d0d88a3d2bbbbd96563
Margarita Sanz <37813342+Msanzdelrio@users.noreply.github.com>
2024-03-11 17:30:24 +0000

    Add city name to weather response

--- app/views.py
+++ app/views.py
@@ -7,7 +7,6 @@ def index():
     weather = None
 
     if request.method == 'POST':
-        # Get the city from the form
         city = request.form['city']
 
         url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid=b5268fcd2ca37ab2198170fac2825453'
@@ -16,6 +15,7 @@ def index():
 
         weather = {
             'country': response['sys']['country'],
+            'city': response['name'],
             'temperature': response['main']['temp'],
             'description': response['weather'][0]['description'],
             'icon': response['weather'][0]['icon'],
